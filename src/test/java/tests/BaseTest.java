6073905aa453b56f64f8fde8f248cc4a15573bc0
Charu Verma <charu.verma0204@gmail.com>
2026-05-25 12:39:18 +0530

    Fix Selenium headless CI issue

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,11 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import utils.DriverFactory;
 
 public class BaseTest {
@@ -13,6 +14,16 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
+    	//WebDriver driver = new ChromeDriver(options);
+    	ChromeOptions options = new ChromeOptions();
+
+    	options.addArguments("--headless=new");
+    	options.addArguments("--no-sandbox");
+    	options.addArguments("--disable-dev-shm-usage");
+    	options.addArguments("--window-size=1920,1080");
+    	WebDriverManager.chromedriver().setup();
+
+    	
         driver = DriverFactory.initializeDriver("chrome");
         driver.get("https://opensource-demo.orangehrmlive.com/");
     }
